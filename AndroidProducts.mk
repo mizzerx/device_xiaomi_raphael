896ab3ff9316d50f9027bde1f40685972edd9134
Jorge Lucas <jorgelucas.sd@gmail.com>
2022-04-05 17:34:24 -0300

    raphael: PixelExperience Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_raphael.mk
+    $(LOCAL_DIR)/aosp_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_raphael-userdebug \
-    lineage_raphael-eng
+    aosp_raphael-userdebug \
+    aosp_raphael-eng
